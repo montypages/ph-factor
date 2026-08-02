f71830763f092be67938078c93f0b483b3b1b8d0
Monty Pages <matmontypages@gmail.com>
2026-08-01 22:12:31 -0700

    added mdsvex for md files.

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,12 @@
+import { mdsvex } from 'mdsvex';
 import adapter from '@sveltejs/adapter-netlify';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 export default {
-	preprocess: vitePreprocess(),
-
+	preprocess: [vitePreprocess(), mdsvex({ extensions: ['.svx', '.md'] })],
 	compilerOptions: {
 		runes: ({ filename }) => (filename.split(/[/\\]/).includes('node_modules') ? undefined : true)
 	},
-
-	kit: {
-		adapter: adapter()
-	}
+	kit: { adapter: adapter() },
+	extensions: ['.svelte', '.svx', '.md']
 };
